6da7cc84b0805788a6721fce678def54ce2da79d
Jiho Han <mlpzaq123456@naver.com>
2019-05-13 16:37:32 +0900

    add comment

--- todo.js
+++ todo.js
@@ -2,24 +2,15 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
+/*
+querySelector
+제공한 선택자 또는 선택자 뭉치와 일치하는 문서 내 첫 번째 Element를 반환합니다. 
+일치하는 요소가 없으면 null을 반환합니다.
+*/
+
 const TODOS_LS = "toDos";
 
 let toDos = [];
-
-function deleteToDo(event) {
-  const btn = event.target;
-  const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDdos = toDos.filter(function(toDo) {
-    return toDo.id !== parseInt(li.id);
-  });
-  toDos = cleanToDdos;
-  saveToDos();
-}
-
-function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
 
 function paintToDo(text) {
   const li = document.createElement("li");
@@ -41,13 +32,12 @@ function paintToDo(text) {
   saveToDos();
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
-  toDoInput.value = "";
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); 
 }
-
+//localStorage 객체는 만료 날짜가없는 데이터를 저장합니다.
+//localStorage 속성은 읽기 전용입니다.
+//JavaScript 값이나 객체를 JSON 문자열로 변환합니다.
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
@@ -56,6 +46,24 @@ function loadToDos() {
       paintToDo(toDo.text);
     });
   }
+}
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDdos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDdos;
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); //이벤트를 취소할 수 있는 경우, 이벤트의 전파를 막지않고 그 이벤트를 취소합니다.
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function init() {
